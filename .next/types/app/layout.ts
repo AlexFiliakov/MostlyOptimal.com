0e43aa45424ac8c7ff51e8a5cb40914edcddd91b
Alex Filiakov <alexfiliakov@gmail.com>
2025-10-04 09:22:16 -0400

    Update file paths in layout and page type definitions to reflect new project structure

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\alexf\OneDrive\Documents\Projects\ErgodicAdvantage.com\ergodicityadvantage.com\app\layout.tsx
+// File: C:\Users\alexf\OneDrive\Documents\Projects\MostlyOptimal.com\ergodicityadvantage.com\app\layout.tsx
 import * as entry from '../../../app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
