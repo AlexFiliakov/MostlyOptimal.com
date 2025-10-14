06eafb9b82a31fa60f00a091889f94047d5c3275
s4nyx <sanyscott4139@gmail.com>
2025-10-15 00:16:24 +0800

    chore: ctasection & definitionsection

--- app/components/CTASection.tsx
+++ app/components/CTASection.tsx
@@ -1,4 +1,6 @@
+'use client';
 import Image from "next/image";
+import CustomButton from "./CustomButton";
 
 export default function CTASection() {
   return (
@@ -29,18 +31,21 @@ export default function CTASection() {
         </p>
 
         <div className="flex flex-col sm:flex-row gap-4 justify-center">
-          <a
+          {/* <a
             href="https://mostlyoptimal.com/tutorial"
             className="inline-block bg-sage-green hover:bg-sage-green/90 text-white font-semibold px-8 py-4 rounded-lg transition-all duration-300 transform hover:scale-105 shadow-lg"
-          >
+          > */}
+          <CustomButton>
+
             Start Your Analysis
-          </a>
-          <a
+          </CustomButton>
+          {/* <a
             href="https://mostlyoptimal.com/research"
             className="inline-block bg-transparent border-2 border-sage-green text-sage-green hover:bg-sage-green hover:text-white font-semibold px-8 py-4 rounded-lg transition-all duration-300"
-          >
+          > */}
+          <CustomButton>
             Research Paper
-          </a>
+          </CustomButton>
         </div>
       </div>
     </section>
