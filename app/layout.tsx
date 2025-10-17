c0f007ded0996a8c14271b0f3a7fc9c601ed363d
Alex Filiakov <alexfiliakov@gmail.com>
2025-10-16 22:04:11 -0400

    Google Analytics tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import Script from 'next/script'
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -36,6 +37,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-6CFY9Q60DQ"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-6CFY9Q60DQ');
+          `}
+        </Script>
+      </head>
       <body className="font-body antialiased">
         {children}
       </body>
